332f4d3db38338383c35ead0d35654a6f5efb6cb
pj8912 <jpintofun2b@gmail.com>
2022-06-30 17:25:50 +0530

    added redirection url for deletnotes

--- notes/deleteNotes.php
+++ notes/deleteNotes.php
@@ -9,9 +9,10 @@ if (isset($_REQUEST['nid']) && isset($_REQUEST['bid'])) {
 
     $sql = "DELETE FROM notes WHERE n_id='$nid' and book_id='$bid'";
     $db->query($sql);
-
+    $uri = $_REQUEST['uri'];
+    $host = $_SERVER['HTTP_POST'];
     // $sql = "SELECT * FROM notes WHERE "
-    header("Location: createnotes.php?bid=$bid");
+    header("Location: $host$uri");
     exit();
 }
 ?>
