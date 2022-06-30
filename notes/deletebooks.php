8ab5d57442dbe487e5df888ef51f579d1fe8ff3f
pj8912 <jpintofun2b@gmail.com>
2022-06-30 19:10:12 +0530

    delete notebooks

--- notes/deletebooks.php
+++ notes/deletebooks.php
@@ -1,15 +1,28 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-if(isset($_GET['bid'])){
-	$bid = $_GET['bid'];
+if(isset($_REQUEST['bid'])){
+
+	$bid = $_REQUEST['bid'];
+	
 	require_once '../database/sql.php';
-	$db = new database('../notesapp.db');
-	$db->query('BEGIN');
-	$s1 = "DELETE FROM books WHERE b_id = '$bid'";
-		$db->query($s1);
-	$s2 = "DELETE FROM notes WHERE b_id = '$bid'";
-		$db->query($s2);
-	$db->query('COMMIT');
+	
+	$db = new database('../notesapp.db', SQLITE3_OPEN_READWRITE);
+	$db->exec('BEGIN;');
+	
+	$s1 = "DELETE FROM notebooks WHERE nb_id = :nb_id ";
+	$stmt = $db->prepare($s1);
+	$stmt->bindParam(':nb_id', $bid);
+	$stmt->execute();
+	
+	$s2 = "DELETE FROM notes WHERE book_id =:book_id";
+	$stmt = $db->prepare($s2);
+	$stmt->bindParam(':book_id', $bid);
+	$stmt->execute();
+	
+	$db->exec('COMMIT;');
+	
 	header('Location: ../index.php');
 	exit();
 
