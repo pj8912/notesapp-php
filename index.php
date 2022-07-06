33f0dc46a0bbe484c5583145f7c16ec05a8ad6a2
pj8912 <jpintofun2b@gmail.com>
2022-07-06 18:46:33 +0530

    front page ui update

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ include_once "includes/navbar.php";
 
 		<div class=" p-5 card card-body col-md-4 text-center m-2">
 			<a class="h4 texts" href="notebooks/create.php">
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-journal-plus" viewBox="0 0 16 16">
+				<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-journal-plus" viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z" />
 					<path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
 					<path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
@@ -37,7 +37,7 @@ include_once "includes/navbar.php";
 View Notes</div> -->
 
 		<div class="p-5 m-2 card card-body col-md-4 text-center">
-			<a href="notebooks/view_notebooks.php" class="h4 texts"> View Notebooks </a>
+			<a href="notebooks/view_notebooks.php" class="h4 texts"> See Notebooks </a>
 		</div>
 		<!-- <div class="p-5 m-2 card card-body col-md-4 text-center">
 			<a href="notebooks/view_notebooks.php" class="h4 texts"> View Notebooks</a>
--- notebooks/view_notebooks.php
+++ notebooks/view_notebooks.php
@@ -11,19 +11,8 @@ include_once '../includes/navbar.php';
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <div class="mt-1 row  ">
-        <!-- <p class="h2">Notebooks</p> -->
-        <div class="">
-           
-            <!-- <a href="create.php" class="rounded-5 btn btn-success btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-plus" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"></path>
-                    <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"></path>
-                    <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"></path>
-                </svg>
-                Create Notebook
-            </a> -->
-        </div> 
+    <div class="mt-2 row ">
+    
         <?php
 
         require '../database/sql.php';
