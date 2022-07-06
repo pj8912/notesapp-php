095c63382fa667ef24124c29589e2cdedc1cd3c3
pj8912 <jpintofun2b@gmail.com>
2022-07-06 18:13:45 +0530

    added book icon

--- notebooks/view_notebooks.php
+++ notebooks/view_notebooks.php
@@ -10,10 +10,10 @@ include_once '../includes/navbar.php';
 <div class="container">
 
 
-    <div class="mt-5 row  p-4">
+    <div class="mt-1 row  ">
         <!-- <p class="h2">Notebooks</p> -->
         <div class="">
-            <h2 class="m-2">NoteBooks</h2>
+           
             <!-- <a href="create.php" class="rounded-5 btn btn-success btn-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-plus" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"></path>
@@ -33,9 +33,15 @@ include_once '../includes/navbar.php';
         // $num = $response->ro`
         while ($row  = $response->fetchArray(SQLITE3_ASSOC)) {
 
-            echo '<div class=" m-2 col-md-4 card card-body rounded-0" style="">';
-            echo '<a href="../notes/createnotes.php?bid='.$row['nb_id'].'" class="text-center h4">' . $row['name'] . '</a>';
-            echo '<div class="mt-2 text-secondary">' . $row['desc'] . '</div><br>';
+            echo '<div class=" m-2 col-md-4 card card-body " style="">';
+	    echo '<a href="../notes/createnotes.php?bid='.$row['nb_id'].'" class="text-center h4" style="text-decoration:none">
+		    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-journal-richtext" viewBox="0 0 16 16">
+  <path d="M7.5 3.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0zm-.861 1.542 1.33.886 1.854-1.855a.25.25 0 0 1 .289-.047L11 4.75V7a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 7v-.5s1.54-1.274 1.639-1.208zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+  <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+  <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+</svg>
+		    ' . $row['name'] . '</a>';
+            echo '<div class="mt-2 text-secondary" style="font-size:12px">' . $row['desc'] . '</div><br>';
             echo '<div class="m-auto">
             <a href="../notes/viewnotes.php?bid='.$row['nb_id'].'" class="btn btn-success rounded-5">View Notes</a> 
             <a href="../notes/createnotes.php?bid='.$row['nb_id'].'" class="btn btn-primary rounded-5">Create/Edit Notes</a>
